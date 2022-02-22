44810bf7a6769af9266e746b4d43671a8e2a516c
汉升 <fuyang.shen@dingxiang-inc.com>
2022-02-22 08:57:02 +0800

    feat: babel base

---
+++ babel/lib/test.js
@@ -0,0 +1,32 @@
+"use strict";
+
+var _classCallCheck2 = require("babel-runtime/helpers/classCallCheck");
+
+var _classCallCheck3 = _interopRequireDefault(_classCallCheck2);
+
+var _createClass2 = require("babel-runtime/helpers/createClass");
+
+var _createClass3 = _interopRequireDefault(_createClass2);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var a;
+a = 3;
+
+var b = function b() {
+  return a + 3;
+};
+
+var F = function () {
+  function F() {
+    (0, _classCallCheck3.default)(this, F);
+
+    this.a = 3;
+  }
+
+  (0, _createClass3.default)(F, [{
+    key: "A",
+    value: function A() {}
+  }]);
+  return F;
+}();
--- babel/src/test.js
+++ babel/src/test.js
@@ -1,4 +1,13 @@
 var a 
 a = 3
 
-const b = () => a + 3
+const b = () => a + 3 
+
+class F {
+  constructor() {
+    this.a = 3
+  }
+
+  A () {}
+}
+
