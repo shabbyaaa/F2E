c5abd5a1dbb5c1d936ea092dc228316018208f19
汉升 <fuyang.shen@dingxiang-inc.com>
2022-03-20 23:50:18 +0800

    feat: update file

--- vue-test/src/App.vue
+++ vue-test/src/App.vue
@@ -11,7 +11,7 @@
   <div>
     <router-link to="/">首页</router-link>
     <router-link to="/about">关于</router-link>
-  </div>
+  </div> 
   <router-view></router-view>
 </template>
 
--- vue-test/src/pages/home.vue
+++ vue-test/src/pages/home.vue
@@ -1,3 +1,8 @@
 <template>
-  <h1>b</h1>
+  <h1>index</h1>
+  <TodoList2 />
 </template>
+
+<script setup>
+import TodoList2 from '../components/TodoList2.vue'
+</script>
